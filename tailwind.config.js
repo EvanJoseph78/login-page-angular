27116caa85db017a14ff95bef18d94c8a7526b12
evan <evandromariano49@gmail.com>
2024-04-10 14:12:49 -0300

    feat: create inital construction

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': {
+          '50': '#ecf4ff',
+          '100': '#dceaff',
+          '200': '#c0d7ff',
+          '300': '#9abbff',
+          '400': '#7294ff',
+          '500': '#516eff',
+          '600': '#3243f9',
+          '700': '#2633dc',
+          '800': '#212db2',
+          '900': '#1d2672',
+          '950': '#151a51',
+        },
+      },
+    },
+    plugins: [],
+  }
+}
